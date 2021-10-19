76a95b447d2a0936f4dd47314fb752b3bd05ea53
Utsav Barnwal <u.barnwal36@gmail.com>
2021-10-20 02:11:23 +0530

    add conversion counter

--- TemperatureConverter/ViewController.swift
+++ TemperatureConverter/ViewController.swift
@@ -10,25 +10,33 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var txtTemperature: UITextField!
+    @IBOutlet weak var labelCountConversions: UILabel!
     @IBOutlet weak var labelResult: UILabel!
+    
+    var count = 0;
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
+    
     @IBAction func onConvertClicked(_ sender: Any) {
         if let temperature = txtTemperature.text {
             if(temperature != "") {
                 if let num = Double(temperature) {
                     labelResult.text = String(convertToFerhanite(temperature: num))
+                    incrementConversionCount()
                 }
             } else {
                 labelResult.text = "Meh! You need to enter some value"
             }
         }
     }
-
+    
+    func incrementConversionCount() {
+        count += 1
+        labelCountConversions.text = String(count) + " Conversion(s)"
+    }
+    
     func convertToFerhanite(temperature: Double) -> Double {
         return temperature * (9/5) + 32
     }
