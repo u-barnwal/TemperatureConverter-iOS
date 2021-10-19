69f6ee85bcf5d34627df273481b23d09676ee316
Utsav Barnwal <u.barnwal36@gmail.com>
2021-10-20 02:03:42 +0530

    design the UI and add conversion functionality

--- TemperatureConverter/ViewController.swift
+++ TemperatureConverter/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var txtTemperature: UITextField!
+    @IBOutlet weak var labelResult: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func onConvertClicked(_ sender: Any) {
+        if let temperature = txtTemperature.text {
+            if(temperature != "") {
+                if let num = Double(temperature) {
+                    labelResult.text = String(convertToFerhanite(temperature: num))
+                }
+            } else {
+                labelResult.text = "Meh! You need to enter some value"
+            }
+        }
+    }
+
+    func convertToFerhanite(temperature: Double) -> Double {
+        return temperature * (9/5) + 32
+    }
 }
 
